b59b83190acae3c1cfb8fff71315609d862b8eac
Yana Yanchenko <yana.yanchenko88@yandex.by>
2021-09-26 21:23:12 +0300

    Sprint10/1

--- src/components/ImagePopup.jsx
+++ src/components/ImagePopup.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
 const ImagePopup = ({card, onClose}) => {
+  React.useEffect(()=>{
+  
+    const handleEscapeClosePopup = (evt) =>{
+      if (evt.keyCode === 27){
+        onClose()
+      }
+    }
+    window.addEventListener('keydown', handleEscapeClosePopup)
+    return () => {
+      window.removeEventListener('keydown', handleEscapeClosePopup)
+    }
+  })
+
   return (
-      <div className={`popup popup_type_photo ${card ? 'popup_opened' : "" } `}>
-      <figure className="popup__place-image">
+      <div className={`popup popup_type_photo ${card ? 'popup_opened' : "" } `} onClick={onClose}>
+      <figure className="popup__place-image" onClick={(evt) => {evt.stopPropagation()}}>
         <img className="popup__image" src={ card && card.link } alt={card && card.name}/>
         <button className="popup__button-close" type="button" name="close" aria-label="закрыть" onClick={onClose} />
         <figcaption className="popup__caption">{card && card.name}</figcaption>
